8cad237a9219a766fed8aacbe0c5d2d44c272654
Kayla Mountain <kaylamt24@gmail.com>
2022-11-23 22:31:16 -0600

    push changes 1

---
+++ public/main.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('button')
+
+const clickHandler = () => alert('The cat name is: Rascal')
+
+btn.addEventListener('click', clickHandler)
+
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+
+const cors = require('cors')
+
 const app = express()
-app.listen(4000, () => console.log(`server running on 4000`))
+
+app.use(express.json())
+app.use(cors())
 
 
 app.use(express.static(`${__dirname}/public`))
+
+app.listen(4000, () => console.log(`server running on 4000`))
 
